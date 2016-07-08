8ae4e29a848053d162af5c3e5d9f2a604c4ad312
Kevin Nguyen <account@kevinwin.com>
2016-07-08 15:25:43 -0700

    Pass tests for recursive reverse string

--- 01_Array_and_Strings/02_reverseStr.js
+++ 01_Array_and_Strings/02_reverseStr.js
@@ -2,7 +2,26 @@
 Reverse string
 */
 
+'use strict';
+
 function reverseStr(str) {
+  let result = '';
+  // build a string by iterating backwards
+  for (let i = str.length-1; i >= 0; i--) {
+    let char = str[i];
+    result += char;
+  }
+  return result;
 };
+
+function recursiveReverseStr(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+  return recursiveReverseStr(str.slice(1)) + str[0];
+}
+
+// uncomment to use recursive solution
+// reverseStr = recursiveReverseStr;
 
 module.exports = reverseStr;
