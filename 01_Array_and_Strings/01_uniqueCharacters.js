52776327fd489487e25554158efebb994ec58299
Kevin Nguyen <account@kevinwin.com>
2016-07-08 15:21:27 -0700

    Pass tests for unique characters

--- 01_Array_and_Strings/01_uniqueCharacters.js
+++ 01_Array_and_Strings/01_uniqueCharacters.js
@@ -3,7 +3,31 @@ Implement an algorithem to determine if a string has all unique characters.
 What if you cannot use additional data structure?
 */
 
-function uniqueCharacters(str) {
+'use strict';
+
+function uniqueCharactersWithDataStructure(str) {
+  const unique = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (unique[char]) {
+      return false;
+    }
+    unique[char] = char;
+  }
+  return true;
 }
+
+function uniqueCharacters(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char, i+1) !== -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// uncomment to use data structure solution
+// uniqueCharacters = uniqueCharactersWithDataStructure;
 
 module.exports = uniqueCharacters;
