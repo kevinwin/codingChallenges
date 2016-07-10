3a71732d661bec3fcb0223aed1d1e1174fcee187
Kevin Nguyen <account@kevinwin.com>
2016-07-09 20:56:19 -0700

    Pass tests for string compression

--- 01_Array_and_Strings/05_stringCompression.js
+++ 01_Array_and_Strings/05_stringCompression.js
@@ -3,9 +3,34 @@ Perform basic string compression
  input: 'aabcccccaaa'
  output: 'a2b1c5a3'
 
+ Extra credit: Do string compression in place
+
 */
 
+'use strict';
+
 function stringCompression(str) {
+  let prevCharCount;
+  let result = '';
+
+  // iterate through length of string
+  for (let i = 0; i < str.length; i++) {
+    let prevChar = str[i];
+    prevCharCount = prevCharCount || 1;// track prevCharCount
+    let nextChar = str[i+1];
+
+    if (prevChar !== nextChar) {
+      result += prevChar + prevCharCount;
+      prevCharCount = 0;
+
+    } else if (prevChar === nextChar) {
+      prevCharCount++
+    }
+  }
+
+  return str.length > result.length ? result : str;
 }
+
+console.log(stringCompression('aabcccccaaa'))
 
 module.exports = stringCompression;
