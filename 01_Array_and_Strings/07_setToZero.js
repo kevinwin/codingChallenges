9021b38f884cbb24c01311ea31d1aa2a9eb5e0f3
Kevin Nguyen <account@kevinwin.com>
2016-07-10 18:27:40 -0700

    Pass tests for set to zero

--- 01_Array_and_Strings/07_setToZero.js
+++ 01_Array_and_Strings/07_setToZero.js
@@ -1,9 +1,42 @@
 /*
 write an algorithm such that if an element in an MxN matrix is 0, the entire row and column are set to 0
 */
+'use strict';
 
 function setToZero(matrix) {
+  var zeroified = {row: {}, col: {}};
 
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      if (zeroified['row'][i] || zeroified['col'][j]) {
+        continue;
+      }
+      let el = matrix[i][j];
+      if (el === 0) {
+        zeroifyRow(matrix, i);
+        zeroifyCol(matrix, j);
+        zeroified['row'][i] = true;
+        zeroified['col'][j] = true;
+      }
+    }
+  }
+
+  return matrix;
 };
+
+function zeroifyRow(matrix, row) {
+  for (let j = 0; j < matrix[row].length; j++) {
+    matrix[row][j] = 0;
+  }
+}
+
+function zeroifyCol(matrix, col) {
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i][col] = 0;
+  }
+}
 
 module.exports = setToZero;
