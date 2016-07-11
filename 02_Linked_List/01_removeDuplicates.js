a23df233a5fa245d41e4b988877502092da7dab1
Kevin Nguyen <account@kevinwin.com>
2016-07-10 19:46:28 -0700

    Pass tests (w / hash table)

--- 02_Linked_List/01_removeDuplicates.js
+++ 02_Linked_List/01_removeDuplicates.js
@@ -3,9 +3,36 @@ Write code to remove duplicates from an unsorted linked list
 FOLLOW UP
 How would you solve this problem if no hash table is not allowed?
 */
+'use strict';
 
 function removeDuplicates(linkedList){
+  let node = linkedList.head;
+  const unique = {};
+  const order = [];
+
+  while (node) {
+    if (!unique[node.value]) {
+      unique[node.value] = node;
+      order.push(node.value);
+    }
+    node = node.next;
+  }
+
+  // Construct linked list
+  let list = {};
+
+  list.head = unique[order.shift()]
+  node = list.head;
+
+  while (order.length) {
+    node.next = unique[order.shift()];
+    node = node.next;
+  }
+
+  return list;
 
 };
+
+// removeDuplicates without hash table
 
 module.exports = removeDuplicates;
