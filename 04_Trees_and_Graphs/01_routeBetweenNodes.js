2f52208ab0c5cda1ac38f31ed776376b4eaa690b
Kevin Nguyen <account@kevinwin.com>
2016-07-17 16:44:30 -0700

    Pass tests for route between nodes

--- 04_Trees_and_Graphs/01_routeBetweenNodes.js
+++ 04_Trees_and_Graphs/01_routeBetweenNodes.js
@@ -7,6 +7,18 @@
 
 function hasRoute(graph, fromNode, toNode) {
 
+  if (fromNode === toNode) {
+    return true;
+  }
+
+  for (let i = 0; i < fromNode.children.length; i++) {
+    let childNode = graph.nodes[fromNode.children[i]];
+    if (hasRoute(graph, childNode, toNode)) {
+      return true;
+    }
+  }
+  return false;
+
 }
 
 module.exports = hasRoute;
