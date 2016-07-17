2b410e680d7b4a4ad91537afb065c3effd18c319
Kevin Nguyen <account@kevinwin.com>
2016-07-17 14:42:05 -0700

    Pass tests for last kth element

--- 02_Linked_List/02_findLastkth.js
+++ 02_Linked_List/02_findLastkth.js
@@ -2,8 +2,22 @@
 Given the head of a linkedList, implement an algorith to find the kth of last element of a singly linked List
 */
 
-function findLastkth(linkedListHead, k) {
+'use strict';
 
+function findLastkth(linkedListHead, k) {
+  let fast = linkedListHead;
+  let slow = linkedListHead;
+
+  for (let i = 0; i < k; i++) {
+    fast = fast.next;
+  }
+
+  while (fast) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  return slow;
 };
 
 module.exports = findLastkth;
