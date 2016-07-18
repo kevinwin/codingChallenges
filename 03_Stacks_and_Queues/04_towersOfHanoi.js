27df5d19397877a28e63f398968a74a1074bd640
Kevin Nguyen <account@kevinwin.com>
2016-07-17 17:34:59 -0700

    Pass tests for towers of hanoi

--- 03_Stacks_and_Queues/04_towersOfHanoi.js
+++ 03_Stacks_and_Queues/04_towersOfHanoi.js
@@ -1,12 +1,18 @@
 /*
-Classic problem of the Towers of Hanoi, you have 3 towers and N disks of different sizes wich can slicde onto any tower. The puzzle starts with disks sorted in asceending order of size from top to bottom(i.e., each disk sits on top of an even larger one). You have the following constraits:
+Classic problem of the Towers of Hanoi, you have 3 towers and N disks of different sizes wich can slicde onto any tower.
+The puzzle starts with disks sorted in asceending order of size from
+top to bottom(i.e., each disk sits on top of an even larger one). You have the following constraits:
 1. only onde disk can be moved at a time.
 2. disk is slid off the top of one tower onton the next tower.
 3. A disk can only be placed on top of a larger disk
 */
 
 function moveTower(N, origin, buffer, target) {
-
+  if (N > 0) {
+    moveTower(N-1, origin, target, buffer);
+    target.push(origin.pop());
+    moveTower(N-1, buffer, origin, target);
+  }
 };
 
 /*
