f783169a03e4f477593e9ec58e61d00721e09e99
Kevin Nguyen <account@kevinwin.com>
2016-07-18 11:03:20 -0700

    Pass tests for triple step

--- 08_recursion_and_dynamic_programming/01_tripleStep.js
+++ 08_recursion_and_dynamic_programming/01_tripleStep.js
@@ -1,5 +1,17 @@
 function tripleStep(n) {
-  // Your code here
+  if (!tripleStep.cache) {
+    tripleStep.cache = [];
+    tripleStep.cache[0] = 1;
+  }
+
+  if (n < 0) {
+    return 0;
+  } else if (tripleStep.cache[n]) {
+    return tripleStep.cache[n];
+  } else {
+    tripleStep.cache[n] = tripleStep(n-1) + tripleStep(n-2) + tripleStep(n-3);
+    return tripleStep.cache[n];
+  }
 }
 
 module.exports = tripleStep;
