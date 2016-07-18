43f9b4a53a80822f5c9f79e76619ab452049cb3e
Kevin Nguyen <account@kevinwin.com>
2016-07-18 12:06:43 -0700

    Pass tests for robot grid

--- 08_recursion_and_dynamic_programming/02_robotGrid.js
+++ 08_recursion_and_dynamic_programming/02_robotGrid.js
@@ -4,9 +4,62 @@
   'off limits' such that the robot cannot step on them. Design an algorithm to find
   a path for the robot from the top left to the bottom right.
 */
+'use strict';
 
 function robotGrid(grid) {
-  // Your code here
+
+  const failedPaths = {};
+  failedPaths.addPoint = function(r, c) {
+    failedPaths[r + ',' + c] = true;
+  };
+
+  const obstacles = {'2,0': true, '1,1': true}
+
+  const path = [];
+
+  function findPath(r, c) {
+
+    // failed path or obstacle - value is 1
+    if (failedPaths[r+','+c] || obstacles[r+','+c]) {
+      return false;
+    }
+
+    // out of bounds
+    if (r > grid.length - 1 || c > grid[0].length - 1) {
+      return false;
+    }
+
+    // at end
+    let atEnd = r === grid.length - 1 && c === grid[0].length - 1;
+
+    if (atEnd || findPath(r+1, c) || findPath(r, c+1)) {
+      path.unshift(r+','+c);
+      return path;
+    }
+
+    failedPaths.addPoint(r, c);
+    return false;
+  }
+
+  return findPath(0, 0)
+
 }
+
+function generateMatrix(r, c) {
+  const matrix = [];
+  for (let i = 0; i < r; i++) {
+    let row = [];
+    for (let j = 0; j < c; j++) {
+      row.push(0);
+    }
+    matrix.push(row);
+  }
+
+  return matrix;
+}
+
+let matrix = generateMatrix(3, 3);
+
+console.log(robotGrid(matrix));
 
 module.exports = robotGrid;
